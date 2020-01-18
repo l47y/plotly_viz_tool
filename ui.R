3eb1d7df139db80c95a21a82a040977c325df8b3
Unknown <nicolaskeller92@googlemail.com>
2020-01-18 20:42:02 +0100

    BUG: linked css correctly to shiny ui

--- ui.R
+++ ui.R
@@ -1,29 +1,42 @@
 source("config.R")
 
 shinyUI(fluidPage(
-  theme = "styles.css",
-  useShinyjs(),  # Include shinyjs
-  
+  includeCSS("www/styles.css"),
+  useShinyjs(),  
   fluidRow(
     
+
+    title = "Basic",
     column(
       4, 
-      fileInput("datainput", "Choose file", multiple = FALSE, accept = NULL, width = NULL),
-      selectInput("datainput2", "Or select from list", choices = c("None", "iris")),
-      uiOutput("plottype")     
+      wellPanel(
+        class = "myPanel",
+        id = "filePanelid",
+        h4("Choose data", class = "panelHeader"),
+        fileInput("datainput", "Choose file", multiple = FALSE, accept = NULL, width = NULL),
+        selectInput("datainput2", "Or select from list", choices = c("None", "iris")),
+        uiOutput("plottype")   
+      )
     ), 
     column(
       4,
-      uiOutput("col1"),
-      uiOutput("col2"),
-      uiOutput("col3")
+      wellPanel(
+        class = "myPanel", 
+        id = "columnPanelid",
+        h4("Select columns"),
+        uiOutput("col1"),
+        uiOutput("col2"),
+        uiOutput("col3")
+      )
     ),
     column(
       4,
       uiOutput("plot_setup")
     )
+      
     
   ),
+    
   
   fluidRow(
     plotlyOutput("mainplot"),
