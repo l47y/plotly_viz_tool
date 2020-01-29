b9b48b09c23d4cc2cc4b4f741876a6e5d2aaad88
l47y <38724058+l47y@users.noreply.github.com>
2020-01-29 16:00:32 +0100

    Delete deployment.R

--- deployment.R
+++
@@ -1,5 +0,0 @@
-library(rsconnect)
-rsconnect::setAccountInfo(name='l47y', token='9A7FE73A33BCED7993D4C0FB83C336DA', 
-                          secret='6/AsYQuu9qllpWjf0nbkEJbdsyMB2CAeUZqOBjWO')
-rsconnect::deployApp('/home/nicolas/Escritorio/RProjects/plotly_viz_tool/')
-
